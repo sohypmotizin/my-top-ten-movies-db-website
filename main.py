1de6b8c54a94ff4ebe2f9333ba981ab77662bb26
Anastasiia <72623937+sohypmotizin@users.noreply.github.com>
2022-02-13 15:50:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired
 import requests
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = '***'
 Bootstrap(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///all_movies.db'
